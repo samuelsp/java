7c94711b5a93d4f953aa7ad8f7b5d7ac01b1a9ef
samuelsp <samp_81@yahoo.com.br>
2014-01-13 21:38:32 -0200

    update FibonacciArray

--- FibonacciArray.java
+++ FibonacciArray.java
@@ -1,21 +1,32 @@
-class FibonacciArray{
-	int[] fib = new int[6];
-	int i = 0;
+public class FibonacciArray{ 
+	int[] array = new int[100];	
+	static int i;
 	
-	void calculaFibonacci(int n){
-		if(n == 0 || n == 1){
-			fib[i] = 1;
-			i += 1;			
-		}else{
-			fib[i] = calculaFibonacci(n - 1) + calculaFibonacci(n - 2);
-			i += 1;
-		}
-	}
-
+	int calculaFibonnaci(int n){
+      	  	
+      if(n == 0){
+         this.array[0] = 1;
+      	 return this.array[0];
+      }
+      
+      else if(n == 1){
+         this.array[1] = 1;
+      	 return this.array[1];
+      }      
+      
+      else{ 
+      	this.array[i] = this.array[n - 1] + this.array[n - 2];      	
+      	return this.array[i];
+      }      
+      
+    }
+  
 	public static void main(String[] args){
-		FibonacciArray f = new FibonacciArray();
-		for(int i = 0; i < 6; i++)
-			f.calculaFibonacci(i);	
+		FibonacciArray fib = new FibonacciArray();
+		for(i = 0; i < 10; i++){			
+			System.out.print(fib.calculaFibonnaci(i) + " ");				
+		}
+		System.out.println();	
 				
 	}		
 
